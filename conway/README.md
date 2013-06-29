94b9fdae9a74fa6ee8f183672a701b7a0998819b
Greg Vaughn <gvaughn@gmail.com>
2013-06-28 20:18:19 -0500

    it works!!!

--- conway/README.md
+++ conway/README.md
@@ -1,3 +1,23 @@
 # Conway
 
-** TODO: Add description **
+My first Elixir program: Conway's Game of Life
+
+In Conway.evolve, it takes a list of 2 element tuples representing x, y
+coordinates of live cells. It returns a list of 2 element tuples
+represeting the new generation, suitable to feed back into the function.
+
+## To Run (the only way I've learned thus far)
+1. Install Elixir
+2. enter iex
+3.   c "lib/conway.ex"
+4. Conway.evolve ...
+
+## Sample inputs
+### Static block
+[{0,1}, {1,1}, {0,0}, {1,0}] returns itself
+### Blinker (2-cycle shape)
+[{1,2}, {1,1}, {1,0}] returns [{0,1},{1,1},{2,1}] which itself returns
+the original
+### Toad (another 2-cycle shape)
+[{1,1}, {2,1}, {3,1}, {0,0}, {1,0}, {2,0}] returns [{2,2}, {0,1}, {3,1},
+{0,0}, {3,0}, {1,-1}] which itself returns the original
--- conway/lib/conway.ex
+++ conway/lib/conway.ex
@@ -1,2 +1,33 @@
 defmodule Conway do
+
+  def evolve(generation) do
+    live_neighbor_stats = generation_stats(generation)
+    Enum.sort(Dict.get(live_neighbor_stats, 3, []) ++
+      intersection(Dict.get(live_neighbor_stats, 2, []), generation))
+  end
+
+  defp intersection(a, b) do
+    :sets.to_list(:sets.intersection(:sets.from_list(a), :sets.from_list(b)))
+  end
+
+  defp generation_stats(live_cells) do
+    counts = live_cells |> Enum.reduce HashDict.new, function(cell_neighbor_counts/2)
+    counts |> Enum.reduce HashDict.new, function(neighbor_count_cells/2)
+  end
+
+  defp cell_neighbor_counts(live_cell, accumulator) do
+    neighbors(live_cell) |> Enum.reduce accumulator, fn(neighbor, acc) ->
+      Dict.update(acc, neighbor, 1, &1 + 1)
+    end
+  end
+
+  defp neighbor_count_cells({cell, count}, collector) do
+    Dict.update(collector, count, [cell], &1 ++ [cell])
+  end
+
+  defp neighbors({x, y}) do
+    [{x - 1 ,y + 1}, {x, y + 1}, {x + 1, y + 1},
+     {x - 1, y},                 {x + 1, y},
+     {x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1}]
+  end
 end
