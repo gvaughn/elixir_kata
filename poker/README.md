5df6e3a2e92e31fdcb3b0eb612e128e0a765179d
Greg Vaughn <gvaughn@gmail.com>
2013-07-08 21:16:31 -0500

    now with Project Euler goodness

--- poker/README.md
+++ poker/README.md
@@ -1,3 +1,11 @@
 # Poker
 
-Determining the winning poker hand
+Determining the winning poker hand:
+
+1. iex -S mix
+2. Poker.euler_calculation
+
+Originally posted in [Project Euler](http://projecteuler.net/problem=54)
+
+I get 376 wins for player 1 and 674 for player 2 which matches
+my [ruby implementation](https://github.com/gvaughn/ruby_kata/tree/master/poker)
--- poker/lib/poker.ex
+++ poker/lib/poker.ex
@@ -6,4 +6,17 @@ defmodule Poker do
     #IO.inspect hand2
     Enum.max([hand1, hand2], fn(hand) -> hand.power end).player
   end
+
+  def euler_calculation do
+    {:ok, file} = File.open "poker.txt"
+    win_list = readline(IO.readline(file), file, [])
+    {p1_wins, p2_wins} = Enum.partition(win_list, &1 == 1)
+    IO.puts "Player 1 wins #{Enum.count(p1_wins)} times"
+    IO.puts "Player 2 wins #{Enum.count(p2_wins)} times"
+  end
+
+  def readline(:eof, _, win_list), do: win_list
+  def readline(line, file, win_list) when is_binary(line) do
+    readline(IO.readline(file), file, [winner(line) | win_list])
+  end
 end
