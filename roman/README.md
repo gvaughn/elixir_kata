45792eb1d9ef1ad0c8cf9c5605879f58048d106a
Greg Vaughn <gvaughn@gmail.com>
2013-07-17 20:14:20 -0500

    Roman Numeral all at once

---
+++ roman/README.md
@@ -0,0 +1,5 @@
+# Roman Numerals
+
+Thanks to @AlbertMoscow we have a different single-file format
+
+Execute via `elixir roman_numerals.exs`
---
+++ roman/roman_numerals.exs
@@ -0,0 +1,45 @@
+defmodule RomanNumerals do
+
+  @moduledoc """
+  Task definition: Create a function taking a positive integer as its parameter and
+  returning a string containing the Roman Numeral representation of that integer.
+  """
+
+  @mappings [{"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}]
+
+  def convert(number) do
+    {_, str} = @mappings |> Enum.reduce {number, ""}, fn
+      ({roman, arabic}, {number, str}) when number >= arabic ->
+        {rem(number, arabic), str <> String.duplicate(roman, div(number, arabic)) }
+      (_, accum) -> accum
+    end
+    str
+  end
+end
+
+ExUnit.start
+
+defmodule RomanNumeralsTest do
+  use ExUnit.Case
+
+  test "converts arabic numbers to roman" do
+    Enum.each [ [1,"I"],
+                [2,"II"],
+                [3,"III"],
+                [4,"IV"],
+                [5,"V"],
+                [6,"VI"],
+                [7,"VII"],
+                [8,"VIII"],
+                [9,"IX"],
+                [10,"X"],
+                [11,"XI"],
+                [14,"XIV"],
+                [19,"XIX"],
+                [20,"XX"],
+                [25,"XXV"],
+                [27,"XXVII"] ], fn [arabic, roman] ->
+          assert roman == RomanNumerals.convert(arabic)
+    end
+  end
+end
