64636e2a6df169ed4d47d20b2f452aad0f959591
Greg Vaughn <gvaughn@gmail.com>
2013-07-27 20:43:10 -0500

    now with visualization

--- conway/README.md
+++ conway/README.md
@@ -2,15 +2,40 @@
 
 My first Elixir program: Conway's Game of Life
 
+# Now with Visualization
+
+I figured out the ANSI escape sequences to draw this on the console.
+Plus, I provided a few interesting starting blobs. First compile:
+
+```
+elixirc "lib/conway.ex"
+```
+Then run can run any of the following:
+
+* elixir -e "Conway.run_glider"
+* elixir -e "Conway.run_spaceship"
+* elixir -e "Conway.run_pulsar"
+
+You could also set your own pairs of x, y coordinates via:
+
+* elixir -e "Conway.run [{1,2}, {1,1}, {1,0}]"
+
 In Conway.evolve, it takes a list of 2 element tuples representing x, y
-coordinates of live cells. It returns a list of 2 element tuples
+coordinates of live cells. It uses the center of the screen as the
+origin (0,0). It returns a list of 2 element tuples
 represeting the new generation, suitable to feed back into the function.
 
-## To Run (the only way I've learned thus far)
-1. Install Elixir
-2. enter iex
-3.   c "lib/conway.ex"
-4. Conway.evolve ...
+##Make It Stop!
+control-C. Twice. It's an Erlang thing apparently.
+
+##Wha' Happened to My CURSOR?
+Yeah. That. Sorry, but I haven't figured it out yet. I tried a System.at_exit call to restore it, but no joy. This should get that cursor back:
+
+```
+echo -e "\033[?25h"
+```
+
+or else open a new terminal window
 
 ## Sample inputs
 ### Static block
--- conway/lib/conway.ex
+++ conway/lib/conway.ex
@@ -1,8 +1,49 @@
 defmodule Conway do
-  #NOTE: a parallel Ruby version can be found
-  # https://github.com/gvaughn/ruby_kata/blob/master/conway/conway.rb
+
+  def run_glider do
+    run([{-24,-22},{-23,-22},{-22,-22},{-22,-23},{-23,-24}])
+  end
+
+  def run_spaceship do
+    run([{-24,0},{-23,0},{-22,0},{-21,0},{-21,-1},{-21,-2},{-22,-3},{-25,-1},{-25,-3}])
+  end
+
+  def run_pulsar do
+    quadrant = [{2,1},{3,1},{4,1},{1,2},{1,3},{1,4},{2,6},{3,6},{4,6},{6,2},{6,3},{6,4}]
+    start = lc {x,y} inlist quadrant, do: [{x,y},{-x,y},{x,-y},{-x,-y}]
+    run(List.flatten(start))
+  end
+
+  def run(generation) do
+    hide_cursor
+    System.at_exit(fn(_code) -> show_cursor end)
+    #TODO write a separate process to do IO.gets and sends a quit signal to this one
+    # when any key is pressed and restore the cursor
+    _run(generation)
+  end
+
+  defp _run(generation) do
+    clear_screen
+    print(generation)
+    :timer.sleep 100
+    _run(evolve(generation))
+  end
+
+  defp clear_screen, do: IO.write "\e[2J"
+  defp hide_cursor,  do: IO.write "\e[?25l"
+  defp show_cursor,  do: IO.write "\e[?25h"
+
+  defp print([head|tail], str // "*") do
+    print(head, str)
+    print(tail, str)
+  end
+
+  defp print([], _), do: nil
+  defp print({x,y}, str), do: IO.write "\e[#{y + 25};#{2 * (x + 25)}H#{str}"
 
   def evolve(generation) do
+    #NOTE: a parallel Ruby version of the core evolve logic can be found
+    # https://github.com/gvaughn/ruby_kata/blob/master/conway/conway.rb
     live_neighbor_stats = generation_stats(generation)
     Enum.sort(Dict.get(live_neighbor_stats, 3, []) ++
       intersection(Dict.get(live_neighbor_stats, 2, []), generation))
