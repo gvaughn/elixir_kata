1a69fa392c976f0b0215ce3935b87f9f38cb1985
Greg Vaughn <gvaughn@gmail.com>
2013-09-19 21:05:21 -0500

    poker updated to remove deprecation warnings

--- poker/lib/poker.ex
+++ poker/lib/poker.ex
@@ -4,12 +4,12 @@ defmodule Poker do
     {hand1, hand2} = Poker.Hand.pair_from_string(hands_binary)
     #IO.inspect hand1
     #IO.inspect hand2
-    Enum.max([hand1, hand2], fn(hand) -> hand.power end).player
+    Enum.max_by([hand1, hand2], fn(hand) -> hand.power end).player
   end
 
   def euler_calculation do
     {:ok, file} = File.open "poker.txt"
-    win_list = Enum.reduce(IO.stream(file), [], fn(line, accum) -> [winner(line) | accum] end)
+    win_list = Enum.reduce(IO.stream(file, :line), [], fn(line, accum) -> [winner(line) | accum] end)
     {p1_wins, p2_wins} = Enum.partition(win_list, &1 == 1)
     IO.puts "Player 1 wins #{Enum.count(p1_wins)} times"
     IO.puts "Player 2 wins #{Enum.count(p2_wins)} times"
