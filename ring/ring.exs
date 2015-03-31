4179a8436b0469cd84a290f320c13375daf9f7a4
Greg Vaughn <gvaughn@gmail.com>
2015-03-31 12:48:05 -0500

    initial version

---
+++ ring/ring.exs
@@ -0,0 +1,27 @@
+defmodule Ring do
+
+  def play(num_players) do
+    _play(0..num_players)
+  end
+
+  defp _play([winner]), do: winner
+  defp _play([winner, _loser]), do: winner
+  defp _play([_loser1, winner, _loser2]), do: winner
+  defp _play(players) do
+    survivors = players
+    |> Stream.chunk(3, 3, [])
+    |> Enum.flat_map(fn triple -> Stream.drop(triple, 1) end)
+
+    _play([0| survivors])
+  end
+end
+
+ExUnit.start
+
+defmodule RingTest do
+  use ExUnit.Case
+
+  test "does sample" do
+    assert 1 == Ring.play(6)
+  end
+end
