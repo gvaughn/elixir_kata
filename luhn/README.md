f51cb09967f57d4af9989f9054918d3cb75c882b
Greg Vaughn <gvaughn@gmail.com>
2016-02-10 17:27:18 -0600

    add luhn algorithm

---
+++ luhn/README.md
@@ -0,0 +1,12 @@
+# Luhn Algorithm
+
+It's an algorithm of check-digits for credit card numbers. It's by no
+means perfect, but catches a number of typical typographic errors.
+[Wikipedia](https://en.wikipedia.org/wiki/Luhn_algorithm) has a good
+writeup.
+
+It came up on the Elixir Slack channel and I decided to give it a try
+just for fun.
+
+# Execute
+elixir waterflow.exs
---
+++ luhn/luhn.exs
@@ -0,0 +1,27 @@
+defmodule Luhn do
+
+  def valid?(cc) do
+    {_, sum} = String.to_integer(cc)
+        |> Integer.digits
+        |> Enum.reverse
+        |> Enum.chunk(2, 2, [0])
+        |> Enum.flat_map_reduce(0, fn([a,b], sum) -> {[], sum + a + Enum.sum(Integer.digits(b*2))} end)
+    rem(sum, 10) == 0
+  end
+end
+
+ExUnit.start
+
+defmodule LuhnTest do
+  use ExUnit.Case
+
+  test "checks known valid number from wikipedia" do
+    assert Luhn.valid?("79927398713")
+  end
+
+  test "checks transposed digits of known valid number from wikipedia" do
+    refute Luhn.valid?("97927398713")
+  end
+
+end
+
