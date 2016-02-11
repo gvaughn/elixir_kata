08c137c837b129744248c3927a72a1a78cf09ab3
Greg Vaughn <gvaughn@gmail.com>
2016-02-10 18:02:09 -0600

    spaces are ok

--- luhn/luhn.exs
+++ luhn/luhn.exs
@@ -8,8 +8,8 @@ defmodule Luhn do
     rem(sum, 10) == 0
   end
 
-  def valid?(cc) do
-    case Integer.parse(cc) do
+  def valid?(cc) when is_binary(cc) do
+    case String.replace(cc, " ", "") |> Integer.parse do
       {cc_int, _} -> valid?(cc_int)
       _ -> {:error, "Cannot parse integer from string"}
     end
@@ -31,6 +31,10 @@ defmodule LuhnTest do
 
   test "what happens with invalid integer input?" do
     assert {:error, _} = Luhn.valid?("abc123")
+  end
+
+  test "spaces are ok" do
+    assert Luhn.valid?("7992 7398 713")
   end
 
 end
