e6bb7f9b513e361d497131e2ffa9d04eee3ff7e5
Greg Vaughn <gvaughn@gmail.com>
2019-11-13 17:22:48 -0600

    upon @pmarrek sharing his solution

--- golf/caesar.exs
+++ golf/caesar.exs
@@ -4,10 +4,26 @@ Code.require_file("golf.exs", __DIR__)
 defmodule Caesar do
   use Golf
 
-  # If we didn't have to handle spaces in the input
-  # {_,[n,p]}=:io.fread"",'~d,~s';IO.puts for c<-p,do: 97+rem c-71-n,26
   def doit() do
     # comments are ignored
-    [n,p]=String.split IO.gets(""),",";IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-String.to_integer(n),26
+    [n,p]=String.split IO.gets"",",";IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-String.to_integer(n),26
   end
+
+  def henrik() do
+    [n,p]=IO.gets("")|>String.split",";IO.puts for<<c<-p>>,do: c<97&&c||97+rem c-71-String.to_integer(n),26
+  end
+
+  def peter_marrek() do
+    {n,","<>p}=IO.gets("")|>Integer.parse;IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-n,26
+  end
+
+  def tweaked_peter_marrek() do
+    {n,","<>p}=Integer.parse IO.gets"";IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-n,26
+  end
+
+  # If we didn't have to handle spaces in the input
+  def no_input_spaces() do
+    {_,[n,p]}=:io.fread"",'~d,~s';IO.puts for c<-p,do: 97+rem c-71-n,26
+  end
+
 end
