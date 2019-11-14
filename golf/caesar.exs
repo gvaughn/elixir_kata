8f3762f00f5de9172efe5e167fb7951c6c2c6335
Greg Vaughn <gvaughn@gmail.com>
2019-11-14 10:40:00 -0600

    fix the lunacy of the foramtter

--- golf/caesar.exs
+++ golf/caesar.exs
@@ -5,18 +5,15 @@ defmodule Caesar do
   use Golf
 
   def winner() do
-    [n, p] = IO.gets("") |> String.split(",")
-    IO.puts(for <<c <- p>>, do: (c < 97 && c) || 97 + rem(c - 71 - String.to_integer(n), 26))
+    [n,p]=IO.gets("")|>String.split",";IO.puts for<<c<-p>>,do: c<97&&c||97+rem c-71-String.to_integer(n),26
   end
 
   def peter_marreck() do
-    {n, "," <> p} = IO.gets("") |> Integer.parse()
-    IO.puts(for <<c <- p>>, do: (c < ?a && c) || ?a + rem(c - 71 - n, 26))
+    {n,","<>p}=IO.gets("")|>Integer.parse;IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-n,26
   end
 
   def greg_tweaked_peter_marreck() do
-    {n, "," <> p} = Integer.parse(IO.gets(""))
-    IO.puts(for <<c <- p>>, do: (c < ?a && c) || ?a + rem(c - 71 - n, 26))
+    {n,","<>p}=Integer.parse IO.gets"";IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-n,26
   end
 
   # If we didn't have to handle spaces in the input
