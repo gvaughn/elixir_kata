927877a8e96c4f3232ede6fc68bb9744a01750f4
Greg Vaughn <gvaughn@gmail.com>
2019-11-14 10:35:53 -0600

    tweak to golf/caesar_test.exs for metaprogramming and shebang

--- golf/caesar.exs
+++ golf/caesar.exs
@@ -4,26 +4,23 @@ Code.require_file("golf.exs", __DIR__)
 defmodule Caesar do
   use Golf
 
-  def doit() do
-    # comments are ignored
-    [n,p]=String.split IO.gets"",",";IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-String.to_integer(n),26
+  def winner() do
+    [n, p] = IO.gets("") |> String.split(",")
+    IO.puts(for <<c <- p>>, do: (c < 97 && c) || 97 + rem(c - 71 - String.to_integer(n), 26))
   end
 
-  def henrik() do
-    [n,p]=IO.gets("")|>String.split",";IO.puts for<<c<-p>>,do: c<97&&c||97+rem c-71-String.to_integer(n),26
+  def peter_marreck() do
+    {n, "," <> p} = IO.gets("") |> Integer.parse()
+    IO.puts(for <<c <- p>>, do: (c < ?a && c) || ?a + rem(c - 71 - n, 26))
   end
 
-  def peter_marrek() do
-    {n,","<>p}=IO.gets("")|>Integer.parse;IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-n,26
-  end
-
-  def tweaked_peter_marrek() do
-    {n,","<>p}=Integer.parse IO.gets"";IO.puts for<<c<-p>>,do: c<?a&&c||?a+rem c-71-n,26
+  def greg_tweaked_peter_marreck() do
+    {n, "," <> p} = Integer.parse(IO.gets(""))
+    IO.puts(for <<c <- p>>, do: (c < ?a && c) || ?a + rem(c - 71 - n, 26))
   end
 
   # If we didn't have to handle spaces in the input
-  def no_input_spaces() do
-    {_,[n,p]}=:io.fread"",'~d,~s';IO.puts for c<-p,do: 97+rem c-71-n,26
-  end
-
+  # def no_input_spaces() do
+  #   {_,[n,p]}=:io.fread"",'~d,~s';IO.puts for c<-p,do: 97+rem c-71-n,26
+  # end
 end
--- golf/caesar_test.exs
+++ golf/caesar_test.exs
@@ -1,23 +1,36 @@
+#! /usr/bin/env elixir
+# execute with `./caesar_test.exs`
+
 Code.require_file("caesar.exs", __DIR__)
 ExUnit.start()
+
 defmodule CaesarGolf do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  test "caesar example" do
-    input = "3,abc defg hijklmnopqrstuvwxyz"
-    expected = "xyz abcd efghijklmnopqrstuvw\n"
+  describe "caesar example" do
+    # for each public function in Caesar module
+    for f <- :functions |> Caesar.__info__() |> Keyword.keys() do
 
-    output = capture_io([input: input, capture_prompt: false], fn ->
-      retval = Caesar.doit()
+      test f do
+        input = "3,abc defg hijklmnopqrstuvwxyz"
+        expected = "xyz abcd efghijklmnopqrstuvw"
+
+        assert expected == output_of(Caesar, unquote(f), input)
+      end
+    end
+  end
+
+  def output_of(mod, fun, input) do
+    capture_io([input: input, capture_prompt: false], fn ->
+      retval = apply(mod, fun, [])
+      # allow test access to the return value when necessary
+      #   retval = receive do
+      #     {:retval, retval} -> retval
+      #   end
       send self(), {:retval, retval}
     end)
-
-    retval = receive do
-      {:retval, retval} -> retval
-    end
-
-    # NOTE output captures the trailing newline from IO.puts
-    assert output == expected || retval == expected
+    |> String.trim_trailing()
+    # NOTE output captures the trailing newline from IO.puts so we trim it
   end
 end
